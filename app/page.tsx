c29875a73d55bc44c3b4e7b64ea5353518efccba
Vulchi Vijaya Kumar <vulchi.vijay@gmail.com>
2026-04-21 13:54:27 +0530

    new sections added

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
       <HowItWorks />
       <Features />
       <SavoniusRotor />
+      <Savonius3D />
       <UseCases />
       <Benefits />
       <Services />
